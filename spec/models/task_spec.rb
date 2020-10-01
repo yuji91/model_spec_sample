13e3aada73d7d081a37df49200e9ce96b9653cbe
YUJI ITO <yuji91.1227@gmail.com>
2020-10-02 02:22:39 +0900

    Add testcase

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "バリデーション" do
+    it "タイトルが未入力の場合、タスクのバリデーションが無効であること" do
+      task = Task.new(title: nil, description: "test", status: :todo, deadline: Time.current)
+      expect(task).to be_invalid
+      expect(task.errors.full_messages).to eq ["Title can't be blank"]
+    end
+    it "ステータスが未入力の場合、タスクのバリデーションが無効であること" do
+      task = Task.new(title: "test", description: "test", status: nil, deadline: Time.current)
+      expect(task).to be_invalid
+      expect(task.errors.full_messages).to eq ["Status can't be blank"]
+    end
+    it "完了期限が未入力の場合、タスクのバリデーションが無効であること" do
+      task = Task.new(title: "test", description: "test", status: :todo, deadline: nil)
+      expect(task).to be_invalid
+      expect(task.errors.full_messages).to eq ["Deadline can't be blank"]
+    end
+    it "完了期限が過去の日付の場合、タスクのバリデーションが無効であること" do
+      task = Task.new(title: "test", description: "test", status: :todo, deadline: Time.current.yesterday)
+      expect(task).to be_invalid
+      expect(task.errors.full_messages).to eq ["Deadline must start from today."]
+    end
+    it "完了期限が今日の日付の場合、タスクのバリデーションが有効であること" do
+      task = Task.new(title: "test", description: "test", status: :todo, deadline: Time.current)
+      expect(task).to be_valid
+    end
+  end
 end
